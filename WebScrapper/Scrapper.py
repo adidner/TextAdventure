75cffad388e3feffe68919344918a555f9b3261e
adidner <adidner@gmail.com>
2020-03-25 00:55:04 -0700

    updated script to include both choiceURL/keys and choiceTitles/text

--- WebScrapper/Scrapper.py
+++ WebScrapper/Scrapper.py
@@ -5,20 +5,20 @@ import json
 #the initial room of my current story is the following URL/ID combination
 overarchingProject = {
     "baseURL": "https://infinite-story.com",
-    "StoryName": "InnKeeper",
+    "StoryName": "Necromancer",
     "Author": "EndMaster",
-    "LinkToStorySite": "https://infinite-story.com/story/info.php?id=2349",
+    "LinkToStorySite": "https://infinite-story.com/story/info.php?id=3108",
     "LinkToProfile": "https://infinite-story.com/profile/?id=5396",
-    "Description": "Running the family business isn't easy!",
+    "Description": "",
     "StoryDateCreated": "12/28/2005",
-    "Choices": "85",
-    "Rooms": "75",
+    "Choices": "144",
+    "Rooms": "107",
     "RoomKey": {}
 }
 
 baseURL = "https://infinite-story.com/"
 queue = []
-initialRoom = "story/room.php?id=87587"
+initialRoom = "story/room.php?id=46933"
 queue.append(initialRoom)
 
 '''
@@ -27,7 +27,7 @@ roomname: {
     currentChoiceURL: ""
     title: "",
     body: "",
-    choices: [""],
+    choices: [{choiceText: "", choiceURL: ""}],
 }
 '''
 
@@ -55,26 +55,28 @@ while len(queue) > 0:
     try:
         for link in choices.find_all('a'):
             choiceURL = link.get('href')
-            links.append(choiceURL)
+            choiceText = link.get_text()
+            links.append({"choiceURL": choiceURL, "choiceText": choiceText})
             if choiceURL not in RoomKey.keys():
                 queue.append(choiceURL)
     except:
-        print("something went wrong")
+        print("End of Path Reached")
         print(currentChoiceURL)
         print(currentRoomURL)
 
     currentObject = {
         "currentRoomURL": currentRoomURL,
         "body": body,
-        "choiceURLs": links,
+        "choices": links,
     }
     RoomKey[currentChoiceURL] = currentObject
-    with open('result.json', 'w') as fp:
-        json.dump(RoomKey, fp)
+
 
 
 overarchingProject["RoomKey"] = RoomKey
 
+with open('StoryKey.json', 'w') as fp:
+    json.dump(overarchingProject, fp)
 
 # with open('result.json', 'a') as fp:
 #     json.dump(overarchingProject, fp)
