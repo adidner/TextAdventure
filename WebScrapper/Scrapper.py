4c6fcc60a20467add2cdf7d24c8dd6639804767c
Aaron Didner <didnera@oregonstate.edu>
2020-06-02 18:52:15 -0700

    updated script to create folder and created imagination

--- WebScrapper/Scrapper.py
+++ WebScrapper/Scrapper.py
@@ -1,25 +1,65 @@
 from bs4 import BeautifulSoup
 import urllib.request as ur
 import json
+import os
+import time
+
+'''
+When redoing a new app,
+change the overarchingProject spec below to match what it should be,
+new inital room, StoryName, ect
+run
+take the json file it creates and copy the contents into src/data/StoryKey.json in the mobile app code
+then change the APP.JSON stuff to be correct
+'''
 
 #the initial room of my current story is the following URL/ID combination
 overarchingProject = {
-    "baseURL": "https://infinite-story.com",
-    "StoryName": "Necromancer",
+    "baseURL": "https://infinite-story.com/",
+    "StoryName": "Imagination",
     "Author": "EndMaster",
-    "LinkToStorySite": "https://infinite-story.com/story/info.php?id=3108",
+    "LinkToStorySite": "https://infinite-story.com/story/info.php?id=2323",
     "LinkToProfile": "https://infinite-story.com/profile/?id=5396",
     "Description": "",
-    "StoryDateCreated": "12/28/2005",
-    "Choices": "144",
-    "Rooms": "107",
-    "RoomKey": {}
+    "StoryDateCreated": "11/28/2005",
+    "Choices": "31",
+    "Rooms": "32",
+    "RoomKey": {},
+    "initialRoom": "story/room.php?id=41162",
 }
 
-baseURL = "https://infinite-story.com/"
+'''
+overarchingProject = {
+    "baseURL": ,
+    "StoryName": ,
+    "Author": ,
+    "LinkToStorySite": ,
+    "LinkToProfile": ,
+    "Description": ,
+    "StoryDateCreated": ,
+    "Choices": ,
+    "Rooms": ,
+    "RoomKey": ,
+    "initialRoom": ,
+}
+
+'''
+
+
+baseURL = overarchingProject["baseURL"]
 queue = []
-initialRoom = "story/room.php?id=46933"
+initialRoom = overarchingProject["initialRoom"]
 queue.append(initialRoom)
+
+
+
+os.system("mkdir " + ".\\" + overarchingProject["StoryName"])
+
+filepath = os.path.join(".", overarchingProject["StoryName"], overarchingProject["StoryName"] + "Spec.json")
+
+with open(filepath, "w") as f:
+    json.dump(overarchingProject,f)
+
 
 '''
 Room key object defintion
@@ -30,6 +70,8 @@ roomname: {
     choices: [{choiceText: "", choiceURL: ""}],
 }
 '''
+
+
 
 RoomKey = {
 
@@ -77,10 +119,7 @@ while len(queue) > 0:
 
 overarchingProject["RoomKey"] = RoomKey
 
-with open('StoryKey.json', 'w') as fp:
+
+
+with open("./"+ overarchingProject["StoryName"] + "/" + overarchingProject["StoryName"]+'.json', 'w') as fp:
     json.dump(overarchingProject, fp)
-
-# with open('result.json', 'a') as fp:
-#     json.dump(overarchingProject, fp)
-
-#print(overarchingProject)
